287f02851a943480dc81fdf02e77d1b51c2dd92b
Ignacio Toledo <itoledoc@gmail.com>
2015-05-08 12:19:43 -0300

    fix exception

--- cycle_tools.py
+++ cycle_tools.py
@@ -268,7 +268,7 @@ def sel(df, lst, limitbands, array, out):
         lambda row: sim(lst, limitbands, row), axis=1)
     try:
         SB_UID = r.query('obs == True').sort('clo').SB_UID.values[0]
-    except IndexError:
+    except IndexError or AttributeError:
         SB_UID = None
 
     if SB_UID is None:
